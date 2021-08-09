234bbb502a2ae593b1f31dada441fa84ba8cfd23
drsimplegraffiti <abayomiogunnusi@gmail.com>
2021-08-09 05:11:54 +0100

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import { Lines } from 'react-preloaders2';
 import Main from './components/Main';
 import Navbar from './components/Navbar'; 
-import Navbar2 from './components/Navbar2'; 
 import Footer from './components/Footer';
-import React,{useState} from 'react';
 
 const App = () => {
-    const [hide, setHide] = useState();
     return (
         <>
         <Navbar />
