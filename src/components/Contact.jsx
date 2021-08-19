11d317e71a7041dc536dfd00eb4772df502b27b2
drsimplegraffiti <abayomiogunnusi@gmail.com>
2021-08-19 10:38:58 +0100

    emailjs used on contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,17 @@
 import Zoom from 'react-reveal/Zoom';
-import { useForm, ValidationError } from '@formspree/react';
-
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
-    const [state, handleSubmit] = useForm("xrgryvyw");
-  if (state.succeeded) {
-      return <p>Thanks for contacting!</p>;
-  }
+    function sendEmail(e) {
+        e.preventDefault();
+
+        emailjs.sendForm('service_zyzcfot',
+            'template_1753n84',
+            e.target,
+            'user_bpXSBo6dceZos4r9c1omo').then(res => {
+                console.log(res);
+            }).catch(err => console.log(err));
+}
     return (
         <Zoom>
             <section className="contact section" id="contact">
@@ -15,22 +20,16 @@ const Contact = () => {
                 
 
                 <div className="contact__container bd-grid">
-                    <form onSubmit={handleSubmit}  action="/https://formspree.io/f/xjvjpedw" method="POST" className="contact__form">
+                    <form onSubmit={sendEmail}  action="" method="POST" className="contact__form">
                            
                         <input type="text" placeholder="Name" className="contact__input" name="name" />
-                        <input type="email" placeholder="Email" className="contact__input" name="email" />
-                        <ValidationError 
-        prefix="Email" 
-        field="email"
-        errors={state.errors}
-      />
+                        <input type="email" placeholder="Email" className="contact__input" name="user_email" />
+                        
                         <textarea name="message" id="message" cols="0" rows="10" className="contact__input" placeholder="Type message here..."></textarea>
-                          <ValidationError 
-        prefix="Message" 
-        field="message"
-        errors={state.errors}
-      />
-                        <input type="button" disabled={state.submitting} value="Submit" className="contact__button button" />
+                        <input 
+                            type="submit"
+                            value="Send"
+                            className="contact__button button"></input>
                     </form>
                 </div>
             </section>
